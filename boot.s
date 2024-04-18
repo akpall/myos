a5042f9060bf9780d6b067b57c0f71a3fd27afa2
Allen-Kristjan Päll <allenkristjanpall@gmail.com>
2024-04-19 01:09:19 +0300

    boot.s: added print_hex function

--- boot.s
+++ boot.s
@@ -1,8 +1,8 @@
 	.code16
 	.global _start
 _start:
-	mov $hello, %si
-	call print_string
+	mov $0x1fb6, %dx
+	call print_hex
 
 	jmp .
 
@@ -18,11 +18,49 @@ print_string:
 	call print
 	jmp print_string
 
+print_hex:
+	mov $hex_start, %si
+	call print_string
+
+	mov %dx, %ax
+	and $0xf000, %ax
+	shr $12, %ax
+	add $hex_codes, %ax
+	mov %ax, %si
+	mov (%si),%ax
+	call print
+
+	mov %dx, %ax
+	and $0xf00, %ax
+	shr $8, %ax
+	add $hex_codes, %ax
+	mov %ax, %si
+	mov (%si),%ax
+	call print
+
+	mov %dx, %ax
+	and $0xf0, %ax
+	shr $4, %ax
+	add $hex_codes, %ax
+	mov %ax, %si
+	mov (%si),%ax
+	call print
+
+	mov %dx, %ax
+	and $0xf, %ax
+	add $hex_codes, %ax
+	mov %ax, %si
+	mov (%si),%ax
+	call print
+
 return:
 	ret
 
-hello:
-	.asciz "hello"
+hex_start:
+	.asciz "0x"
+
+hex_codes:
+	.ascii "0123456789abcdef"
 
 	.space 510 - (. - _start)
 	.short 0xaa55
