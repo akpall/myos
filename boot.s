7f622c4ea81e7a1b8148dd4ad3a0cd062c6bd6e8
Allen-Kristjan Päll <allenkristjanpall@gmail.com>
2024-04-18 19:09:23 +0300

    boot.s: removed duplicate mov

--- boot.s
+++ boot.s
@@ -1,22 +1,19 @@
 .code16
 _start:
 	mov $0x0e, %ah
+
 	mov $'H', %al
 	int $0x10
 
-	mov $0x0e, %ah
 	mov $'e', %al
 	int $0x10
 
-	mov $0x0e, %ah
 	mov $'l', %al
 	int $0x10
 
-	mov $0x0e, %ah
 	mov $'l', %al
 	int $0x10
 
-	mov $0x0e, %ah
 	mov $'o', %al
 	int $0x10
 
