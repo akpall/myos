0c42068d18ba034a1d4517b564318039a7732dd3
Allen-Kristjan Päll <allenkristjanpall@gmail.com>
2024-04-21 04:26:12 +0300

    boot.s: split functions into asm folder

---
+++ asm/disk.s
@@ -0,0 +1,27 @@
+BOOT_DRIVE:
+	.byte 0
+
+DISK_ERROR_MSG:
+	.asciz "Disk read error!"
+
+disk_error:
+	mov $DISK_ERROR_MSG, %si
+	call print_string
+	jmp .
+
+disk_load:
+	push %dx
+	;;
+	mov $0x02, %ah
+	mov %dh, %al
+	mov $0, %ch
+	mov $0x02, %cl
+	mov $0, %dh
+	int $0x13
+	;;
+	jc disk_error
+	;;
+	pop %dx
+	cmp %al, %dh
+	jne disk_error
+	ret
---
+++ asm/print.s
@@ -0,0 +1,60 @@
+HEX_CODES:
+	.ascii "0123456789abcdef"
+
+HEX_START:
+	.asciz "0x"
+
+print:
+	mov $0x0e, %ah
+	int $0x10
+	ret
+
+print_hex:
+	mov $HEX_START, %si
+	call print_string
+	;;
+	mov %dx, %ax
+	and $0xf000, %ax
+	shr $12, %ax
+	add $HEX_CODES, %ax
+	mov %ax, %si
+	mov (%si),%ax
+	call print
+	;;
+	mov %dx, %ax
+	and $0xf00, %ax
+	shr $8, %ax
+	add $HEX_CODES, %ax
+	mov %ax, %si
+	mov (%si),%ax
+	call print
+	;;
+	mov %dx, %ax
+	and $0xf0, %ax
+	shr $4, %ax
+	add $HEX_CODES, %ax
+	mov %ax, %si
+	mov (%si),%ax
+	call print
+	;;
+	mov %dx, %ax
+	and $0xf, %ax
+	add $HEX_CODES, %ax
+	mov %ax, %si
+	mov (%si),%ax
+	call print
+	;;
+	mov $' ', %al
+	call print
+	;;
+	ret
+
+print_string:
+	lodsb
+	or %al, %al
+	jz print_string_null
+	call print
+	jmp print_string
+
+print_string_null:
+	ret
