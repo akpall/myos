71cfa067328066306d03c950561a630f30675290
Allen-Kristjan Päll <allenkristjanpall@gmail.com>
2024-04-21 03:01:24 +0300

    boot.s: test reading drive

--- boot.s
+++ boot.s
@@ -1,7 +1,22 @@
 	.code16
 	.global _start
 _start:
-	mov $0x1fb6, %dx
+	mov %dl, BOOT_DRIVE
+
+	mov $0x8000, %bp
+	mov %bp, %sp
+
+	mov $0x9000, %bx
+	mov $2, %dh
+	mov BOOT_DRIVE, %dl
+	call disk_load
+
+	mov $0x9000, %si
+	mov (%si), %dx
+	call print_hex
+
+	mov $0x9000+512, %si
+	mov (%si), %dx
 	call print_hex
 
 	jmp .
@@ -91,5 +106,11 @@ HEX_CODES:
 DISK_ERROR_MSG:
 	.asciz "Disk read error!"
 
+BOOT_DRIVE:
+	.byte 0
+
 	.space 510 - (. - _start)
 	.short 0xaa55
+
+	.fill 256, 2, 0xdada
+	.fill 256, 2, 0xface
